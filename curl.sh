67b196abab01c74e2266f763d51669fb46c9c9fc
Simeon Arabov <s.arabov@myxrobotics.com>
2023-05-12 22:22:37 +0300

    dalle

---
+++ curl.sh
@@ -0,0 +1,8 @@
+url https://api.openai.com/v1/images/generations \                       ✔  18s  
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer <API_HERE>" \
+  -d '{
+    "prompt": "a photo of a happy corgi puppy sitting and facing forward, studio light, longshot",
+    "n":1,
+    "size":"1024x1024"
+   }'
